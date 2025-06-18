82a51101e6ecf39cac1a1164c6fcadd34d470f5a
Jeremy McWilliams <jeremym@lclark.edu>
2025-06-18 15:35:48 -0700

    updated selector class

--- nde-chatwidget-custom.component.ts
+++ nde-chatwidget-custom.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+declare var _slaask: any;
 
 @Component({
   selector: 'custom-nde-chatwidget-custom',
@@ -7,6 +8,24 @@ import { Component } from '@angular/core';
   templateUrl: './nde-chatwidget-custom.component.html',
   styleUrl: './nde-chatwidget-custom.component.scss'
 })
-export class NdeChatwidgetCustomComponent {
+export class NdeChatwidgetCustomComponent implements OnInit {
+  ngOnInit(): void {
+    // Check if script is already added
+    if (!document.querySelector('script[src*="slaask.js"]')) {
+      const script = document.createElement('script');
+      script.type = 'text/javascript';
+      script.src = 'https://libapps.s3.amazonaws.com/sites/5089/include/slaask.js';
+      script.onload = () => {
+        // Wait for the script to load before initializing
+        if (window.hasOwnProperty('_slaask')) {
+          (_slaask as any).init('295dd444a1af2c9ab3456b7da45a415a');
+        }
+      };
+      document.head.appendChild(script);
+    } else {
+      // Script already exists, just init again
+      (_slaask as any).init('295dd444a1af2c9ab3456b7da45a415a');
+    }
+  }
 
 }
